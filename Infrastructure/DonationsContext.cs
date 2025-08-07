d02bdc96189fa3397be9301c1164fa31d0f227fc
Ezequiel <Ezequiel@DESKTOP-VCVMV9H>
2025-08-07 17:52:05 -0300

    Agregar archivos de proyecto.

---
+++ Infrastructure/DonationsContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class DonationsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DonationsContext(DbContextOptions<DonationsContext> options) : base(options)
+        {
+
+        }
+    }
+}
